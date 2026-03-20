38b1b951f51960e14df0a912c6221a370416f4f3
S NIKHIL KUMAR <148072755+nikhilkumar1729@users.noreply.github.com>
2026-03-20 22:32:03 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,13 @@
+int main:
+return 0:
+fact = -1
+return = -2
+return 0 - 1:
+return -1
+int fact < system.out.println>
+reutrn -1
+public class Solution n<return -1>
+return -2:
+res = ans:
+fact = fact { return  - 1}
+reutrn 0:
